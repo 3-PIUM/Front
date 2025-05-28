5e0f3dd6f69e1327e0a0b3cf23a87517639659f2
Ham <jhm010234@naver.com>
2025-05-28 12:01:01 +0900

    style: 기본 컬러 변수 정리

---
+++ src/styles/colors.js
@@ -0,0 +1,11 @@
+const colors = {
+  white: "#ffffff",
+  black: "#000000",
+  mainPink: "#F23477",
+  subPink: "#FFE2EC",
+  lightGrey: "#E6E6E6",
+  mediumGrey: "#ADADAD",
+  darkGrey: "#7F7F7F",
+};
+
+export default colors;
