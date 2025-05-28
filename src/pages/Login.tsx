3d0a228392a562a5016abf74409c91a82c693a23
Ham <jhm010234@naver.com>
2025-05-28 13:25:51 +0900

    feat: 공통 Button 컴포넌트 생성

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,3 +1,10 @@
+import Button from "../components/Button";
+
 export default function Login() {
-  return <div>홈 페이지입니다</div>;
+  return (
+    <div>
+      <div>홈 페이지입니다</div>
+      <Button label="로그인하기" />
+    </div>
+  );
 }
