d1582eee4934c4c9741731c28419d4d2f495309e
Ham <jhm010234@naver.com>
2025-05-29 14:52:40 +0900

    Feat: MenuLayout.tsx 제작

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,10 @@
-import "./App.css";
-import Layout from "./components/Layout";
-import { Route, Routes } from "react-router-dom";
-import Login from "./pages/Login";
-import ProductDetail from "./pages/ProductDetail";
+import { useRoutes } from "react-router-dom";
+import routes from "../src/Routes";
 
-function App() {
-  return (
-    <Layout>
-      <Routes>
-        <Route path="/" element={<Login />} />
-        <Route path="/product-detail" element={<ProductDetail />} />
-      </Routes>
-    </Layout>
-  );
-}
+const App = () => {
+  const content = useRoutes(routes, location);
+
+  return <>{content}</>;
+};
 
 export default App;
---
+++ src/pages/WishList.tsx
@@ -0,0 +1,10 @@
+export default function WishList() {
+  return (
+    <>
+      <div>fd</div>
+      <div>fd</div>
+      <div>fd</div>
+      <div>fd</div>
+    </>
+  );
+}
