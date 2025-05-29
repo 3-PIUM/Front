83fc612760eb042649b06d0e335effeef5c221d8
Ham <jhm010234@naver.com>
2025-05-29 10:08:55 +0900

    Feat: ItemCard 컴포넌트 제작

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,3 +1,4 @@
+import ItemCard from "../components/ItemCard";
 import TextField from "../components/TextField";
 import TextHeader from "../components/TextHeader";
 
@@ -8,6 +9,16 @@ export default function Login() {
       {/* <Button label="로그인하기" /> */}
       <TextHeader pageName="회원가입" />
       <TextField fieldName="비밀번호" />
+      <ItemCard
+        imageSource={
+          "https://image.oliveyoung.co.kr/cfimages/cf-goods/uploads/images/thumbnails/550/10/0000/0022/A00000022465410ko.jpg?l=ko"
+        }
+        itemName={
+          "[5/26하루특가] [올영단독] 메디큐브 부스터프로 쿠로미 에디션(+쿠로미헤드케이스+세안밴드+디바이스클리너+스티커2종)"
+        }
+        discountRate={19}
+        price={339000}
+      />
     </div>
   );
 }
