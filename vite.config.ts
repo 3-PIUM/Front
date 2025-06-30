c66ebad42be3eb20d52eecf0a5bc0b59d8740538
kim-chaeyeon <kimchaeyeon0415@gmail.com>
2025-06-30 16:32:02 +0900

    PWA 적용

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,35 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
 
 export default defineConfig({
-  plugins: [react()],
+  server: {
+    port: 5173,
+  },
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
+      manifest: {
+        name: "PIUM Web App",
+        short_name: "PIUM",
+        start_url: "/",
+        display: "standalone",
+        background_color: "#ffffff",
+        theme_color: "#ffe4f1",
+        icons: [
+          {
+            src: "/icons/icon-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "/icons/icon-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+      },
+    }),
+  ],
 });
